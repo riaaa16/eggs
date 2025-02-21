fd50b2e621c80b503e38eb2a590340d43581dab3
Ria <vg435@njit.edu>
2025-02-20 19:18:43 -0500

    MultiLineChart with separate colors and pointer

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import LineChart from './charts/LineChart';
+import MultiLineChart from './charts/MultiLineChart';
 
 function App() {
   return (
@@ -14,6 +15,28 @@ function App() {
         title={"Average Price of Bread, per lb."}
         subtitle={"White, pan, for Northeast America"}
       />
+      <MultiLineChart
+        csvFiles={[
+              {
+                filepath: "/data/Eggs/Eggs, grade A, large, per doz. in NE avg.csv",
+                product: "eggs"
+              },
+              {
+                filepath: "/data/Bread/Bread, white, pan, per lb. (453.6 gm) in NE avg..csv",
+                product: "bread"
+              },
+              {
+                filepath: "/data/Milk/Milk, fresh, whole, per gal. in NE avg..csv",
+                product: "milk"
+              },
+              {
+                filepath: "data/Potato Chips/Potato chips, per 16 oz. in NE avg..csv",
+                product: "potato chips"
+              }
+          ]}
+          title={"Average Prices of Grocery Items"}
+          subtitle={"For Northeast America"}
+        />
     </div>
   );
 }
